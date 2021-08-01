9dc07abc7ae5dbfd53f763c7dce995d47b8f3661
Natanael <ntnlbg@gmail.com>
2021-08-01 10:36:27 -0300

    Fix grep example

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,7 @@ export const NEW_LINE = "\n";
 
 export const DEFAULT_CONTENT = `
 ${NUKE} exec echo "do something"
-${NUKE} IGNORED exec grep console.log -irl src | awk '{print "☢ open \"" $1 "\""}'
+${NUKE} IGNORED exec grep console.log -irl src | awk '{print "☢ open \\"" $1 "\\""}'
 ${NUKE} IGNORED load "src/**/*.*"
 ${NUKE} IGNORED exec find $(pwd)/src -type f
 ${NUKE} IGNORED file "myNewFolder/myNewFile.txt"
